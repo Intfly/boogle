03e8012e6483d0f975d9e2ae87f0af092f3ad6ff
Arthur123-hub <arthuretrillard@gmail.com>
2024-11-15 18:16:21 +0100

    joueur et dé
    j

---
+++ ConsoleApp1/De.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class De
+    {
+        char[] lettres;
+        char lettre;
+
+        public De(char[] lettres, char lettre)
+        {
+            if(lettres.Length == 6)
+            {
+                this.lettres = lettres;
+            }
+            this.lettre = lettre;
+        }
+        public void Lance(Random r)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Joueur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Joueur
+    {
+        string nom;
+        int score;
+        string[] motsTrouves;
+        public bool Contain(string mot)
+        {
+            bool b = false;
+            for(int i = 0;i < this.motsTrouves.Length;i++)
+            {
+                if(mot == motsTrouves[i])
+                {
+                    b = true;
+                }
+            }
+            return b;
+        }
+        public void Add_Mot(string mot)
+        {
+            this.motsTrouves = new int[motsTrouves.Length + 1];
+            for(int i = 0; i < this.motsTrouves.Length; i++)
+            {
+
+            }
+        }
+    }
+}
