50a5808a539097831626ce6aade526fbd457ba21
Int <valentinfournelval@gmail.com>
2024-11-29 15:36:13 +0100

    définir langue classe jeu

---
+++ ConsoleApp1/Jeu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Jeu
+    {
+        string langue;
+
+        public void definirLangueJeu()
+        {
+
+            string langueTemp = "";
+            while(langueTemp != "français" || langueTemp != "anglais")
+            {
+                Console.WriteLine("veuillez choisir la langue du jeu (anglais ou français)");
+                langueTemp = Console.ReadLine();
+            }
+            switch (langueTemp)
+            {
+                case "français":
+                    this.langue = "FR";
+                    break;
+                case "anglais":
+                    this.langue = "EN";
+                    break;
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,9 @@ class Program
 
     static void Main()
     {
+        Jeu jeu = new Jeu();
+        jeu.definirLangueJeu();
+        
         Plateau plateau = new Plateau();
         plateau.definirTaillePlateau();
 
